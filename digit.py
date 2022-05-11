665e53468b82617034e16103b7f40ab7da1b6eb5
zDestinate <EA.ADM1N.one@gmail.com>
2022-05-11 18:42:20 -0400

    Add files via upload

--- digit.py
+++ digit.py
@@ -6,69 +6,97 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
-from PIL import Image
-
 import tkinter as tk
 import tkinter.simpledialog
 import tkinter.filedialog
 
 
-# MNIST DATASET
-mnist = tf.keras.datasets.mnist
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
+# Load data from file
+train_data = np.genfromtxt("train-data.txt", dtype=float, delimiter=" ")
+X_train = train_data[:,1:]
+Y_train = train_data[:,0].astype(int)
+
+test_data = np.genfromtxt("test-data.txt", dtype=float, delimiter=" ")
+X_test = test_data[:,1:]
+Y_test = test_data[:,0].astype(int)
+
+
+# Convert them into 16x16 image
+x_train = X_train.reshape(7291, 16, 16, 1).astype('float32')
+x_test = X_test.reshape(2007, 16, 16, 1).astype('float32')
+
 
 # Classification
 n_classes = 10
-y_train_class = tf.keras.utils.to_categorical(y_train, n_classes)
+y_train_class = tf.keras.utils.to_categorical(Y_train, n_classes)
+y_test_class = tf.keras.utils.to_categorical(Y_test, n_classes)
 
-# Normalize the train dataset
-#x_train = tf.keras.utils.normalize(X_train, axis=1)
 
-x_train = X_train.reshape(60000, 28, 28, 1).astype('float32')
-#x_test = X_test.reshape(10000, 28, 28, 1).astype('float32')
+# Show training data image
+# plt.figure()
+# for i in range(36):
+    # plt.subplot(6,6,i+1)
+    # plt.tight_layout()
+    # plt.imshow(x_train[i], cmap='gray', interpolation='none')
+    # plt.title("Digit: {}".format(Y_train[i]))
+    # plt.xticks([])
+    # plt.yticks([])
+# plt.show()
 
 
 
 # Architecture
 model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Conv2D(32, (3, 3), input_shape=(28, 28, 1), padding="same", activation="relu"))
+model.add(tf.keras.layers.Conv2D(32, (3, 3), input_shape=(16, 16, 1), padding="same", activation="relu"))
 model.add(tf.keras.layers.MaxPooling2D((2, 2)))
-model.add(tf.keras.layers.Dropout(0.25))
+model.add(tf.keras.layers.Dropout(0.05))
 model.add(tf.keras.layers.Conv2D(64, (3, 3), activation="relu"))
 model.add(tf.keras.layers.MaxPooling2D((2, 2)))
-model.add(tf.keras.layers.Dropout(0.25))
-model.add(tf.keras.layers.Conv2D(64, (3, 3), activation="relu"))
-model.add(tf.keras.layers.MaxPooling2D((2, 2)))
-model.add(tf.keras.layers.Dropout(0.25))
+model.add(tf.keras.layers.Dropout(0.05))
 model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(256, activation="relu"))
+model.add(tf.keras.layers.Dense(128, activation="relu"))
 model.add(tf.keras.layers.Dense(64, activation="relu"))
-model.add(tf.keras.layers.Dropout(0.5))
 model.add(tf.keras.layers.Dense(32, activation="relu"))
 model.add(tf.keras.layers.Dense(10, activation="softmax"))
 model.summary()
-
-#print(model.input_shape)
-#print(model.weights)
-#print(model.output_shape)
-
-#model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 model.compile(optimizer='RMSprop', loss='categorical_crossentropy', metrics=['accuracy'])
 epochvalue = tk.simpledialog.askinteger("Input", "Enter total epoches for training")
 model.fit(x_train, y_train_class, epochs=epochvalue)
 
 
+# Test model eval
+print("\n\nTest model evaluation:");
+results = model.evaluate(x_test, y_test_class)
+print("Test loss: {0}\nTest accuracy: {1}\n\n".format(results[0], results[1]))
 
-tk.Tk().withdraw()
+
+# Manual test model
 while 1:
-    filename = tk.filedialog.askopenfile()
-    if not filename:
-        break
-    img = cv2.imread(filename.name)[:,:,0]
-    img = cv2.resize(img, (28, 28), interpolation = cv2.INTER_LANCZOS4)
-    img = np.invert(np.array([img]))
-    prediction = model.predict(img)
-    plt.title("Predicted Digit: {}".format(np.argmax(prediction)))
-    plt.imshow(img[0], cmap=plt.cm.binary)
+    testmodel_index = tk.simpledialog.askinteger("Input", "Input the index # of the test model\nMax: {0}".format(len(x_test)))
+    prediction = model.predict(x_test[testmodel_index].reshape(1, 16, 16))
+    plt.figure()
+    plt.title("Predicted: {0}\nTruth: {1}".format(np.argmax(prediction), Y_test[testmodel_index]))
+    plt.imshow(x_test[testmodel_index], cmap='gray', interpolation='none')
+    plt.xticks([])
+    plt.yticks([])
+    print("test[{0}] - Predicted: {1}, Truth: {2}".format(testmodel_index, np.argmax(prediction), Y_test[testmodel_index]))
     plt.show()
+
+
+# Test file
+# tk.Tk().withdraw()
+# while 1:
+    # filename = tk.filedialog.askopenfile()
+    # if not filename:
+        # break
+    # img = cv2.imread(filename.name)[:,:,0]
+    # img = cv2.resize(img, (16, 16), interpolation = cv2.INTER_LANCZOS4)
+    # print(np.array([img]))
+    # img = np.invert(np.array([img]))
+    # prediction = model.predict(img)
+    # plt.title("Predicted Digit: {}".format(np.argmax(prediction)))
+    # plt.imshow(img[0], cmap=plt.cm.binary)
+    # plt.show()
 
